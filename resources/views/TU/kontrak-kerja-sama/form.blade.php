65c56caebf286acb158e3e9ae1d4b903a881b362
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2023-10-13 17:34:03 +0800

    Update

--- resources/views/TU/kontrak-kerja-sama/form.blade.php
+++ resources/views/TU/kontrak-kerja-sama/form.blade.php
@@ -1,55 +1,49 @@
-
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('nama') }}</label>
+    <label class="form-label"> {{ Form::label('nama') }}</label>
     <div>
         {{ Form::text('nama', $tuKontrakKerjaSama->nama, ['class' => 'form-control' .
         ($errors->has('nama') ? ' is-invalid' : ''), 'placeholder' => 'Nama']) }}
         {!! $errors->first('nama', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuKontrakKerjaSama <b>nama</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('tanggal_masuk') }}</label>
+    <label class="form-label"> {{ Form::label('tanggal_masuk') }}</label>
     <div>
         {{ Form::text('tanggal_masuk', $tuKontrakKerjaSama->tanggal_masuk, ['class' => 'form-control' .
         ($errors->has('tanggal_masuk') ? ' is-invalid' : ''), 'placeholder' => 'Tanggal Masuk']) }}
         {!! $errors->first('tanggal_masuk', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuKontrakKerjaSama <b>tanggal_masuk</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('asal') }}</label>
+    <label class="form-label"> {{ Form::label('asal') }}</label>
     <div>
         {{ Form::text('asal', $tuKontrakKerjaSama->asal, ['class' => 'form-control' .
         ($errors->has('asal') ? ' is-invalid' : ''), 'placeholder' => 'Asal']) }}
         {!! $errors->first('asal', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuKontrakKerjaSama <b>asal</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('perihal') }}</label>
+    <label class="form-label"> {{ Form::label('perihal') }}</label>
     <div>
         {{ Form::text('perihal', $tuKontrakKerjaSama->perihal, ['class' => 'form-control' .
         ($errors->has('perihal') ? ' is-invalid' : ''), 'placeholder' => 'Perihal']) }}
         {!! $errors->first('perihal', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuKontrakKerjaSama <b>perihal</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('lampiran') }}</label>
+    <label class="form-label"> {{ Form::label('lampiran') }}</label>
     <div>
         {{ Form::text('lampiran', $tuKontrakKerjaSama->lampiran, ['class' => 'form-control' .
         ($errors->has('lampiran') ? ' is-invalid' : ''), 'placeholder' => 'Lampiran']) }}
         {!! $errors->first('lampiran', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuKontrakKerjaSama <b>lampiran</b> instruction.</small>
     </div>
 </div>
 
-    <div class="form-footer">
-        <div class="text-end">
-            <div class="d-flex">
-                <a href="#" class="btn btn-danger">Cancel</a>
-                <button type="submit" class="btn btn-primary ms-auto ajax-submit">Submit</button>
-            </div>
+<div class="form-footer">
+    <div class="text-end">
+        <div class="d-flex">
+            <a href="#" class="btn btn-danger">Cancel</a>
+            <button type="submit" class="btn btn-primary ms-auto ajax-submit">Submit</button>
         </div>
     </div>
+</div>
--- resources/views/TU/surat-tugas/form.blade.php
+++ resources/views/TU/surat-tugas/form.blade.php
@@ -1,55 +1,49 @@
-
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('nama') }}</label>
+    <label class="form-label"> {{ Form::label('nama') }}</label>
     <div>
         {{ Form::text('nama', $tuSuratTuga->nama, ['class' => 'form-control' .
         ($errors->has('nama') ? ' is-invalid' : ''), 'placeholder' => 'Nama']) }}
         {!! $errors->first('nama', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuSuratTuga <b>nama</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('tanggal_masuk') }}</label>
+    <label class="form-label"> {{ Form::label('tanggal_masuk') }}</label>
     <div>
         {{ Form::text('tanggal_masuk', $tuSuratTuga->tanggal_masuk, ['class' => 'form-control' .
         ($errors->has('tanggal_masuk') ? ' is-invalid' : ''), 'placeholder' => 'Tanggal Masuk']) }}
         {!! $errors->first('tanggal_masuk', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuSuratTuga <b>tanggal_masuk</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('asal') }}</label>
+    <label class="form-label"> {{ Form::label('asal') }}</label>
     <div>
         {{ Form::text('asal', $tuSuratTuga->asal, ['class' => 'form-control' .
         ($errors->has('asal') ? ' is-invalid' : ''), 'placeholder' => 'Asal']) }}
         {!! $errors->first('asal', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuSuratTuga <b>asal</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('perihal') }}</label>
+    <label class="form-label"> {{ Form::label('perihal') }}</label>
     <div>
         {{ Form::text('perihal', $tuSuratTuga->perihal, ['class' => 'form-control' .
         ($errors->has('perihal') ? ' is-invalid' : ''), 'placeholder' => 'Perihal']) }}
         {!! $errors->first('perihal', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuSuratTuga <b>perihal</b> instruction.</small>
     </div>
 </div>
 <div class="form-group mb-3">
-    <label class="form-label">   {{ Form::label('lampiran') }}</label>
+    <label class="form-label"> {{ Form::label('lampiran') }}</label>
     <div>
         {{ Form::text('lampiran', $tuSuratTuga->lampiran, ['class' => 'form-control' .
         ($errors->has('lampiran') ? ' is-invalid' : ''), 'placeholder' => 'Lampiran']) }}
         {!! $errors->first('lampiran', '<div class="invalid-feedback">:message</div>') !!}
-        <small class="form-hint">tuSuratTuga <b>lampiran</b> instruction.</small>
     </div>
 </div>
 
-    <div class="form-footer">
-        <div class="text-end">
-            <div class="d-flex">
-                <a href="#" class="btn btn-danger">Cancel</a>
-                <button type="submit" class="btn btn-primary ms-auto ajax-submit">Submit</button>
-            </div>
+<div class="form-footer">
+    <div class="text-end">
+        <div class="d-flex">
+            <a href="#" class="btn btn-danger">Cancel</a>
+            <button type="submit" class="btn btn-primary ms-auto ajax-submit">Submit</button>
         </div>
     </div>
+</div>
