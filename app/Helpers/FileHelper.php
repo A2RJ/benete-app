eb7b7f53ef565f1180725232369d843bbfbe16a0
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2023-10-22 23:21:31 +0800

    Update: Upload to gdrive, check file and download

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -5,12 +5,14 @@ namespace App\Helpers;
 use Error;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Ramsey\Uuid\Uuid;
+use Yaza\LaravelGoogleDriveStorage\Gdrive;
 
 class FileHelper
 {
+    public $GDriveFolder = env('GOOGLE_DRIVE_FOLDER_NAME');
+
     public static function upload(Request $request, string $file, string $folder = ''): string
     {
         try {
@@ -51,6 +53,60 @@ class FileHelper
             }
             if (!Storage::exists($pathToImage)) throw new Error("File $pathToImage is not exist");
             return Storage::download($pathToImage);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public static function checkFileExistsGDrive($fileName)
+    {
+        $folder = self::$GDriveFolder;
+        $data = Gdrive::all('/' . $folder);
+        $fileExists = collect($data)
+            ->where('type', 'file')
+            ->where('path', "$folder/$fileName")
+            ->first();
+
+        return $fileExists;
+    }
+
+    public static function uploadGDrive(Request $request, string $file, string $folder, string $customName)
+    {
+        try {
+            $requestFile = $request->file($file);
+            if (!$requestFile) {
+                throw new Exception("File {$file} is required", 422);
+            }
+
+            $extension = $requestFile->getClientOriginalExtension();
+            $fileName = $folder . '/' . date('Y') . '/' . date('m') . '/' . date('Y-m-d H:i:s') . '-' . $customName . ' ' . time() . '.' . $extension;
+
+            Gdrive::put(self::$GDriveFolder . "/$fileName", $requestFile);
+
+            $check = self::checkFileExistsGDrive($fileName);
+            if (!$check) {
+                throw new Exception("Error white uploading files", 500);
+            }
+            return $fileName;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public static function downloadGDrive($fileName)
+    {
+        try {
+            if (!\request()->hasValidSignature()) {
+                abort(401);
+            }
+            $check = self::checkFileExistsGDrive($fileName);
+            if (!$check) {
+                throw new Exception("Your file is not exist", 500);
+            }
+            $data = Gdrive::get('Benetestssss/docx/docx/FITUR.docx');
+            return response($data->file, 200)
+                ->header('Content-Type', $data->ext)
+                ->header('Content-disposition', 'attachment; filename="' . $data->filename . '"');
         } catch (\Throwable $th) {
             throw $th;
         }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Models\Role;
-use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Yaza\LaravelGoogleDriveStorage\Gdrive;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,42 +20,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-// Route::get('/', function () {
-//     Role::query()->delete();
-//     $role = Role::create([
-//         'name' => 'admin',
-//         'display_name' => 'Administrator',
-//         'description' => 'Administrtor hanya dapat mengelola akun user',
-//     ]);
-//     $user = User::whereEmail('admin@mail.com')->first();
-//     if (!$user->hasRole($role->name)) {
-//         $user->addRole($role);
-//     }
-//     Role::create([
-//         'name' => 'bidang keuangan',
-//         'display_name' => 'Bidang Keuangan',
-//         'description' => 'User ini hanya dapat mengelola bidang keuangan',
-//     ]);
-//     Role::create([
-//         'name' => 'bidang kesyabandaran',
-//         'display_name' => 'Bidang Kesaybandaran',
-//         'description' => 'User ini hanya dapat mengelola bidang kesyabandaran',
-//     ]);
-//     Role::create([
-//         'name' => 'bidang pengelola bmn dan persediaan',
-//         'display_name' => 'Bidang Pengelola BMN dan Persediaan',
-//         'description' => 'User ini hanya dapat mengelola bidang pengelola bmn dan persediaan',
-//     ]);
-//     Role::create([
-//         'name' => 'bidang kepegawaian atau tata usaha',
-//         'display_name' => 'Bidang Kepegawaian atau Tata Usaha',
-//         'description' => 'User ini hanya dapat mengelola bidang kepegawaian atau tata usaha',
-//     ]);
-//     Role::create([
-//         'name' => 'bidang kepelabuhan',
-//         'display_name' => 'Bidang Kepelabuhan',
-//         'description' => 'User ini hanya dapat mengelola bidang kepelabuhan',
-//     ]);
-// });
